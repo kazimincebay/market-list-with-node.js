ac8792a2a043d05dd0d441f0b3c1d6da9cea9681
Kazım İncebay <kazimincebay@gmail.com>
2025-09-19 10:50:22 +0300

    Project Finished
    - Basic CRUD process for your market list
    - I installed express, nodemon and dotenv in this project

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const app = express();
 
+app.use(cors());
 dotenv.config();
 
 const PORT = process.env.PORT;
